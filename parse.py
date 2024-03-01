6bce87d63b17f13edd4fc62aa76c0e9c43b6b7ed
Fiidel <ada.helesic@gmail.com>
2024-03-01 21:02:08 +0100

    exported line parsing to list into a function

--- parse.py
+++ parse.py
@@ -1,18 +1,29 @@
 import sys
 import re
 
-# ==================================
+###################################################
+#####               FUNCTIONS                 #####
+###################################################
+
 # get all non-null lines into a list
 # ==================================
 
-lineList = []
+def ParseLinesToList():
+    lineList = []
 
-for line in sys.stdin:
-    if re.search(r"^[\s]", line) == None:
-        line = line.strip()
-        lineList.append(line)
+    for line in sys.stdin:
+        if re.search(r"^[\s]", line) == None:
+            line = line.strip()
+            lineList.append(line)
 
-# remove useless .IPPcode24 intro
-lineList.remove(".IPPcode24")
+    # remove useless .IPPcode24 intro
+    lineList.remove(".IPPcode24")
+    return lineList
 
-# print(lineList)
+
+###################################################
+#####                  MAIN                   #####
+###################################################
+
+lineList = ParseLinesToList()
+print(lineList)
