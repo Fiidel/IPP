7efeed55f0da49ea775092712b3ad6f43881130e
Fiidel <ada.helesic@gmail.com>
2024-03-01 22:57:06 +0100

    Basis for instruction list (added classes)

--- parse.py
+++ parse.py
@@ -2,6 +2,33 @@ import sys
 import re
 
 ###################################################
+#####                CLASSES                  #####
+###################################################
+
+class Instruction:
+    def __init__(self, opcode, arg1 = None, arg2 = None, arg3 = None):
+        self.opcode = opcode
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.arg3 = arg3
+        self.next = None
+
+
+class InstructionList:
+    def __init__(self):
+        self.head = None
+    def InsertNextInstruction(self, opcode, arg1 = None, arg2 = None, arg3 = None):
+        newInstruction = Instruction(opcode, arg1, arg2, arg3)
+        if self.head is None:
+            self.head = newInstruction
+        else:
+            lastInstruction = self.head
+            while lastInstruction.next != None:
+                lastInstruction = lastInstruction.next
+            lastInstruction.next = newInstruction
+    
+
+###################################################
 #####               FUNCTIONS                 #####
 ###################################################
 
@@ -21,17 +48,19 @@ def ParseLinesToList():
     return lineList
 
 
-def
-
-
 ###################################################
 #####                  MAIN                   #####
 ###################################################
 
 lineList = ParseLinesToList()
 
-print(lineList)
 
-lineList = 
+### INSTRUCTION LIST TESTS
+# iList = InstructionList()
+# iList.InsertNextInstruction("LABEL", "loopStart")
 
-print(lineList)
+# print("printing first instruction:")
+# print(iList.head.opcode)
+# print(iList.head.arg1)
+# print(iList.head.arg2)
+# print(iList.head.arg3)
