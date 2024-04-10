b634af0143d1e6aae1282af813c7e621c5c82c1e
Fiidel <ada.helesic@gmail.com>
2024-04-10 13:57:39 +0200

    refactor: InstructionBase class structure

--- ipp-core/student/Instructions/InstructionEntities/InstructionBase.php
+++ ipp-core/student/Instructions/InstructionEntities/InstructionBase.php
@@ -4,12 +4,19 @@ namespace IPP\Student\Instruction;
 
 abstract class InstructionBase implements IInstruction
 {
+    // PROPERTIES
     private OperationCode $opcode;
     public function getOpcode() : OperationCode
     {
         return $this->opcode;
     }
     public function setOpcode($opcode) : void
+    {
+        $this->opcode = $opcode;
+    }
+
+    // CONSTRUCTOR
+    public function __construct(OperationCode $opcode)
     {
         $this->opcode = $opcode;
     }
