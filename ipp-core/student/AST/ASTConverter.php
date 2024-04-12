c0cd8502b1c0bf7111efb625354b24ce34bebfb0
Fiidel <ada.helesic@gmail.com>
2024-04-12 16:24:54 +0200

    feature: added converter from LinkedList to AST

---
+++ ipp-core/student/AST/ASTConverter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace IPP\Student\AST;
+
+use IPP\Student\LinkedList\LinkedList;
+
+class ASTConverter
+{
+    public function ParseInstructions2AST(LinkedList $instructionList) : AST
+    {
+        $AST = new AST;
+
+        // TODO: missing order number? (this method presumes that once it gets null, that's where the program ends)
+
+        $order = 1;
+        while (true)
+        {
+            $instruction = $instructionList->GetInstructionWithOrder($order);
+            if ($instruction == null)
+            {
+                break;
+            }
+
+            $AST->InsertNext($instruction);
+            $order += 1;
+        }
+
+        return $AST;
+    }
+}
